c598b602f0cad4c1341bfdae62ead6b2691c81ad
mclevins <mclevins@gmail.com>
2024-12-10 16:40:44 -0500

    First project in React Scrimba

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <main>
+      <img src="src/assets/react.svg" alt="React logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  </StrictMode>
 )
+
+
