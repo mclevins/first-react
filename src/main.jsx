0c1e75092b4d97fb7b70fdaec9792aed06358eaa
mclevins <mclevins@gmail.com>
2024-12-11 11:32:08 -0500

    first iteration of simple one page react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <StrictMode>
-    <main>
-      <img src="src/assets/react.svg" alt="React logo" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    <Header />
+    <Page />
+    <Footer />
   </StrictMode>
 )
 
+function Header() {
+  return (
+    <img src="/src/assets/react.svg" />
+  )
+};
 
+function Page() {
+  return (
+    <main>
+      <h1>This is a React App!</h1>
+      <ol>
+        <li>Because it is composable!</li>
+        <li>Because it is declarative!</li>
+        <li>Because Zuck owns it!!! The Zuck!</li>
+      </ol>
+    </main>
+  )
+};
+
+function Footer() {
+  return (
+    <p>&copy; 2024 Levins development. All rights reserved.</p>
+  )
+};
